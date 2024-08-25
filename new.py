5cf90bf82c2bbb380cc4cc55845e4cae03d7cb40
Annisa Sekartierra Mulyanto <141292309+annisatierra@users.noreply.github.com>
2024-08-25 08:27:23 +0000

    bismillah

--- new.py
+++ new.py
@@ -1,8 +1,20 @@
+import subprocess
+import sys
+
+def install_requirements():
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirement.txt"])
+    except subprocess.CalledProcessError as e:
+        print(f"Error installing packages: {e}")
+        sys.exit(1)
+
+# Call the function to install packages
+install_requirements()
+
 import streamlit as st
 import joblib
 import pandas as pd
 import pickle
-import catboost
 from datetime import datetime, date
 
 # Set page configuration
